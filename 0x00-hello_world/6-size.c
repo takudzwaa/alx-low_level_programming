94812f82c868504fd0c4459c2b5b349635be578d
takudzwaa <takuldzwah@gmail.com>
2023-08-10 15:06:41 -0700

    Fix some errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ *  main
+ *
+ *  Return: Always 0 
+ */
 int main(void) {
     printf("Size of a char: %zu byte(s)\n", sizeof(char));
     printf("Size of an int: %zu byte(s)\n", sizeof(int));
