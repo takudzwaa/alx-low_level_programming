139134500217475da497296f238ee728f0e50f6e
takudzwaa <tcflsoftware@gmail.com>
2023-08-11 23:01:37 +0200

    Still fixing some errors

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
+ * main - prints all possible different combinations of 3 digits
  * Return: Always 0 (Success)
  */
 int main(void)
